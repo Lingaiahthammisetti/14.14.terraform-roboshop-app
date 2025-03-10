e3007ad2fffada8ec0812e3cbaa6ae48f642f667
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-03-10 15:54:00 +0530

    terraform roboshop app

--- variables.tf
+++ variables.tf
@@ -1,50 +1,50 @@
-# variable "common_tags" {
-# #   default = {
-# #     Project     = "roboshop"
-# #     Environment = "dev"
-# #     Terraform   = "true"
-# #   }
-# }
-
-# variable "tags" {
-  
-# }
-
-# variable "project_name" {
-#   #default = "roboshop"
-# }
-# variable "environment" {
-#   #default = "dev"
-# }
-
-variable "zone_name" {
-  default = "lingaiah.online"
+variable "common_tags" {
+#   default = {
+#     Project     = "roboshop"
+#     Environment = "dev"
+#     Terraform   = "true"
+#   }
 }
 
-# variable "vpc_id" {
+variable "tags" {
   
-# }
+}
 
-# variable "component_sg_id" {
-  
-# }
+variable "project_name" {
+  #default = "roboshop"
+}
+variable "environment" {
+  #default = "dev"
+}
 
-# variable "private_subnet_ids" {
-  
-# }
+variable "zone_name" {
+  #default = "lingaiah.online"
+}
 
-# variable "iam_instance_profile" {
+variable "vpc_id" {
   
-# }
+}
 
-# variable "app_alb_listener_arn" {
+variable "component_sg_id" {
   
-# }
+}
 
-# variable "rule_priority" {
+variable "private_subnet_ids" {
   
-# }
+}
 
-# variable "app_version" {
+variable "iam_instance_profile" {
   
-# }
+}
+
+variable "app_alb_listener_arn" {
+  
+}
+
+variable "rule_priority" {
+  
+}
+
+variable "app_version" {
+  
+}
